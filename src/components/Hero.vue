a47d8f255b293f99cb0c4d80a2f8b93649f78035
bytesbanana <saharat.paynok@gmail.com>
2023-02-08 22:55:04 +0700

    setup tailwind

---
+++ src/components/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="flex flex-col items-center justify-center p-4 gap-2">
+    <h1 class="text-4xl font-semibold text-gray-100">Speed Typing Test</h1>
+    <p class="text-md text-gray-100">
+      Commodo est reprehenderit reprehenderit anim officia aute consequat
+      incididunt.
+    </p>
+  </section>
+</template>
+
+<script setup></script>
